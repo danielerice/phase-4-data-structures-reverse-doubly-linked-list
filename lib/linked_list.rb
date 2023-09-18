829e2e31d209f020d3a0025ac81f15be0e2fd127
Daniel Rice <danielrice5627@gmail.com>
2023-09-17 18:15:05 -0700

    commit tree 0ba07eefbfdec76843d9258e3fbba59a5abc0970

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -8,7 +8,36 @@ class LinkedList
     @tail = nil
   end
 
+  # def reverse!
+  #   if @head == nil
+  #     return
+  #   end
+    
+  #   curr = @tail
+    
+  #   while curr.prev_node
+  #     curr.next_node = curr.prev_node
+  #   end
+  #   @head = @tail
+  #   @tail = @head
+  # end
+
   def reverse!
-    # your code here
+    return if @head.nil?
+  
+    curr = @head
+    prev = nil
+  
+    while curr
+      next_node = curr.next_node
+      curr.next_node = prev
+      curr.prev_node = next_node
+      prev = curr
+      curr = next_node
+    end
+  
+    @tail = @head
+    @head = prev
   end
+
 end
